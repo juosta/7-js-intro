3fea558469a754c9a13092a6f712facb7b527b49
juosta <79445286+juosta@users.noreply.github.com>
2021-03-09 15:07:39 +0200

    1 and 2(1-4) tasks

---
+++ js/01-variables.js
@@ -0,0 +1,71 @@
+// kintamuju iniciavimas
+// 1
+const a =  3;
+console.log(a);
+
+const b =  8;
+console.log(b);
+
+const c =  11;
+console.log(c);
+
+//2
+const vardas = 'Martynas';
+console.log(vardas);
+
+const pavarde = 'Papartynas';
+console.log(pavarde);
+
+const miestas = 'Kaunas';
+console.log(miestas);
+
+//3
+
+const sarasas1 = [2, 8, 10, 9, 4];
+console.log(sarasas1);
+
+const sarasas2 = [12, 16, 2, 13, 20];
+console.log(sarasas2);
+
+const sarasas3 = [10, 11, 10, 6, 15];
+console.log(sarasas3);
+
+//4
+const abc = ['a', 'b', 'c', 'd', 'e'];
+console.log(abc);
+
+const miestai = ['Kaunas', 'Vilnius', 'Klaipeda', 'Alytus', 'Utena'];
+console.log(miestai);
+
+const vardai = ['Migle', 'Beata', 'Gintare', 'Justina', 'Dovile'];
+console.log(vardai);
+
+//Veiksmai su kintamaisiais
+//1
+
+const suma = a + b + c;
+console.log(suma);
+
+//2
+const duomenys = vardas + ' ' + pavarde + ' ' + miestas;
+console.log(duomenys);
+
+//3
+const sarasas1_verte = sarasas1[0] - sarasas1[1] + sarasas1[2] - sarasas1[3] + sarasas1[4];
+console.log(sarasas1_verte);
+
+const sarasas2_verte = sarasas2[0] - sarasas2[1] + sarasas2[2] - sarasas2[3] + sarasas2[4];
+console.log(sarasas2_verte);
+
+const sarasas3_verte = sarasas3[0] - sarasas3[1] + sarasas3[2] - sarasas3[3] + sarasas3[4];
+console.log(sarasas3_verte);
+
+//3
+const abc_verte = abc[4] + ', ' + abc[3] + ', ' + abc[2] + ', ' + abc[1] + ', ' + abc[0];
+console.log(abc_verte);
+
+const miestai_reiksme = miestai[4] + ', ' + miestai[3] + ', ' + miestai[2] + ', ' + miestai[1] + ', ' + miestai[0];
+console.log(miestai_reiksme);
+
+const vardai_reiksme = vardai[4] + ', ' + vardai[3] + ', ' + vardai[2] + ', ' + vardai[1] + ', ' + vardai[0];
+console.log(vardai_reiksme);
---
+++ js/02-comparison.js
@@ -0,0 +1,104 @@
+// kintamuju palyginimas
+// 1
+console.log("Salyga: ", 'a > b');
+if (a > b) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'a < b');
+if (a < b) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'a == b');
+if (a == b) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'a != b');
+if (a != b) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'a >= b');
+if (a >= b) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'a <= b');
+if (a <= b) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+// 2 
+const vardoilgis = vardas.length;
+const pavardesilgis = pavarde.length;
+const miestoilgis = miestas.length;
+
+console.log('Vardo ilgis: ', vardoilgis);
+console.log('Pavardes ilgis: ', pavardesilgis);
+console.log('Miesto ilgis: ', miestoilgis);
+
+// 3 
+
+console.log("Salyga: ", 'vardo ilgis > pavardes ilgis');
+if (vardoilgis > pavardesilgis) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'vardo ilgis < pavardes ilgis');
+if (vardoilgis < pavardesilgis) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'vardo ilgis == pavardes ilgis');
+if (vardoilgis == pavardesilgis) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'vardo ilgis != pavardes ilgis');
+if (vardoilgis != pavardesilgis) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'vardo ilgis >= pavardes ilgis');
+if (vardoilgis >= pavardesilgis) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+console.log("Salyga: ", 'vardo ilgis <= pavardes ilgis');
+if (vardoilgis <= pavardesilgis) {
+    console.log("pomidoras");
+} else {
+    console.log("Bandykite dar karta")
+}
+
+// 4 
+const miest_sar_ilg = [miestai[0].length, miestai[1].length, miestai[2].length, miestai[3].length, miestai[4].length]
+console.log(miest_sar_ilg);
+const vard_sar_ilg = [vardai[0].length, vardai[1].length, vardai[2].length, vardai[3].length, vardai[4].length]
+console.log(vard_sar_ilg);
+
+// 5
