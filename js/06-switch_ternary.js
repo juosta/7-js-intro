fed82b4eb9084120d85ad3c1efdcd02db512687a
juosta <79445286+juosta@users.noreply.github.com>
2021-03-15 13:25:34 +0200

    swithc and ternary

---
+++ js/06-switch_ternary.js
@@ -0,0 +1,31 @@
+// SWITCH STATEMENT
+// Switch() { case, break, default }
+
+const animal = "cat";
+
+switch (animal) {
+    case 'dog':
+        console.log('Bark bark');
+        break;
+    case 'cat':
+        console.log('Meow meow');
+        break;
+    default:
+        console.log('Unknown animal');
+        break;
+}
+/*
+TERNARY OPERATOR: ? 
+
+palyginimas ? ka daryti jei tenkina logika : ka daryti jei logikos netenkina
+palyginimas ? true : false 
+*/
+
+const a = 3;
+const b = 5;
+
+const ats = a > b ? 'daugiau' : 'ne daugiau';
+console.log(ats);
+
+
+    
