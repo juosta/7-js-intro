303f5b2b1da17fcfb23dacb25e8c93c79d4791da
juosta <79445286+juosta@users.noreply.github.com>
2021-03-25 21:49:10 +0200

    Slaphead

---
+++ js/24-Slaphead.js
@@ -0,0 +1,13 @@
+function bald(x){
+    let count = 0;
+    for (const i in x){ 
+        if( x[i] === "/"){ count++; }
+    }
+    const shaved = x.replace(/\//g, '-');
+    return [shaved, count === 0 ? "Clean!" : count === 1 ? "Unicorn!" : count === 2 ?  "Homer!" : count > 3 && count <=5 ?  "Careless!" : "Hobo!"];
+  }
+
+    console.log(bald('/---------'), ['----------', 'Unicorn!']);
+    console.log(bald('/-----/-'), ['--------', 'Homer!']);
+    console.log(bald('--/--/---/-/---'), ['---------------', 'Careless!']);
+    
